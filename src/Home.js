be69f2be24e37ebede5e8fbf18ed47b85522f147
Gaiseric <sebastiaancrisan@gmail.com>
2022-02-18 18:53:51 +0100

    add reactive variable with usestate

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,17 @@
+import { useState } from "react"
+
 const Home = () => {
-    const handleClick = (e) => {
-        console.log("Btn clicked", e);
-    }
-    const handleClickAgain = (name, e) => {
-        console.log(name, e.target);
+    const [name, setName] = useState("Gankerjack");
+    const [age, setAge] = useState(27);
+    const handleClick = () => {
+        setName("Gaiseric");
+        setAge(28);
     }
     return (
         <div className="home">
             <h2>Homepage</h2>
+                <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('Gankerjack', e)}>Click me again</button>
         </div>
     );
 }

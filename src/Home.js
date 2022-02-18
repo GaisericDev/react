5fc1ac0c5d57c8030b324de945bd756ccf5afad3
Gaiseric <sebastiaancrisan@gmail.com>
2022-02-18 18:47:24 +0100

    add functions and click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log("Btn clicked", e);
+    }
+    const handleClickAgain = (name, e) => {
+        console.log(name, e.target);
+    }
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('Gankerjack', e)}>Click me again</button>
         </div>
     );
 }

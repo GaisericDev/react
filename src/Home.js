2942546e4d2e5d4db7d7e41d34b5e81f650d1b7b
Gaiseric <sebastiaancrisan@gmail.com>
2022-02-21 14:17:41 +0100

    rm delete blogs button

--- src/Home.js
+++ src/Home.js
@@ -4,10 +4,6 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null); 
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog=> blog.id !== id);
-        setBlogs(newBlogs);
-    }
     //runs initially, and then every time the data changes, don't change state here if it's watched because that triggers infinite loop lol
     //add empty dependency array if you only want to run this on initial render
     //put variables in dependency array to watch for changes in those variables only
@@ -23,7 +19,7 @@ const Home = () => {
 
     return (
         <div className="home">
-            {blogs && <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete} />}
+            {blogs && <BlogList blogs={blogs} title="All blogs!" />}
         </div>
     );
 }

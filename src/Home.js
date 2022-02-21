7a9a9c001cd667e932e27d163307e8587ef31a76
Gaiseric <sebastiaancrisan@gmail.com>
2022-02-21 13:56:14 +0100

    bugfix handle delete

--- src/Home.js
+++ src/Home.js
@@ -14,8 +14,7 @@ const Home = () => {
     }
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs" />
-            <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Mario's blogs" />
+            <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete} />
         </div>
     );
 }

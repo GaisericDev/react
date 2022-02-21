7925f42a67939a6ba417d62a22459762e2488c46
Gaiseric <sebastiaancrisan@gmail.com>
2022-02-21 14:04:33 +0100

    add useEffect dependencies

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -8,13 +8,25 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]); 
 
+    const [name, setName] = useState('mario');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog=> blog.id !== id);
         setBlogs(newBlogs);
     }
+    //runs initially, and then every time the data changes, don't change state here because that triggers infinite loop lol
+    //add empty dependency array if you only want to run this on initial render
+    //put variables in dependency array to watch for changes in those variables only
+    useEffect(()=>{
+        console.log("useEffect ran");
+        console.log(name);
+    }, [name]);
+
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete} />
+            <button onClick={() => setName('luigi')}>change name</button>
+            <p>{name}</p>
         </div>
     );
 }
